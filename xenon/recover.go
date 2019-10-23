bb73c39869a9676e2e9a6e7918165ca7da573a20
cisordeng <m@cisor.cn>
2019-10-23 21:59:39 +0800

    recover 输出错误日志

--- xenon/recover.go
+++ xenon/recover.go
@@ -1,7 +1,11 @@
 package xenon
 
 import (
+	"fmt"
+	"runtime"
+
 	"github.com/cisordeng/beego/context"
+	"github.com/cisordeng/beego/logs"
 )
 
 func RecoverPanic(ctx *context.Context) {
@@ -24,6 +28,16 @@ func RecoverPanic(ctx *context.Context) {
 				"innerErrMsg": "",
 			}
 		}
+		logs.Critical("the request url is ", ctx.Input.URL())
+		logs.Critical("Handler crashed with error", err)
+		for i := 1; ; i++ {
+			_, file, line, ok := runtime.Caller(i)
+			if !ok {
+				break
+			}
+			logs.Critical(fmt.Sprintf("%s:%d", file, line))
+		}
+
 		err = ctx.Output.JSON(resp, true, true)
 	}
 }
